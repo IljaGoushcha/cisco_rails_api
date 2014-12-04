019f30e8e399fcd4582d4d40aaac9742bb84bf80
IljaGoushcha <iljagou@yahoo.com>
2014-12-04 09:35:23 -0500

    modified migration and model Object

--- app/models/object.rb
+++ app/models/object.rb
@@ -1,17 +1,17 @@
-# Object might be less than ideal name for a class
+# Object might be less than ideal name for a Class
 # rails g model actually refused to generate a model called object (reserved word)
 class Object < ActiveRecord::Base
-  # before_create :generate_uid
+  before_create :generate_uid
 
-  # private
+  private
 
-  # # I am not sure if I am generating UID or UUID
-  # def generate_uid
-  #   begin
-  #     # .uuid or .hex?
-  #     self.uid = SecureRandom.uuid
-  #   end while self.class.exists?(uid: self.uid)
-  # end
+  # I am not sure if I am generating UID or UUID
+  def generate_uid
+    begin
+      # .uuid or .hex?
+      self.uid = SecureRandom.uuid
+    end while self.class.exists?(uid: self.uid)
+  end
 
 end
 
--- db/migrate/20141203191503_create_objects.rb
+++ db/migrate/20141203191503_create_objects.rb
@@ -1,10 +1,10 @@
 class CreateObjects < ActiveRecord::Migration
   def change
     create_table :objects do |t|
-      # t.string :uid#, unique: true, null: false, index: true
+      t.string :uid, unique: true, null: false, index: true
       t.string :firstName
       t.string :lastName
-      t.date :dob#, null: false
+      t.date :dob, null: false
       t.string :dod
 
       t.timestamp
